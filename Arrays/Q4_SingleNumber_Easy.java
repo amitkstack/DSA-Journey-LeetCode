281abd81b71f9e454399e66e9d18966a49c76d78
Amit Kumar <amitkumar128206@gmail.com>
2026-02-05 22:02:53 +0530

    Add solution for Single Number problem
    Implemented a solution for the Single Number problem using XOR.

---
+++ Arrays/Q4_SingleNumber_Easy.java
@@ -0,0 +1,16 @@
+/**
+ * Question: Single Number (LeetCode #136)
+ * Approach:- This approach leverages the XOR bitwise operator to cancel out duplicate numbers ($x \oplus x = 0$), leaving only the unique element that appears an odd number of times.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+public class Q4_SingleNumber_Easy {
+    public static int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            ans = nums[i] ^ ans;
+        }
+        return ans;
+    }
+}
