f1cc17e56570d3368f885e6c4573eab1ff405fd1
Amit Kumar <amitkumar128206@gmail.com>
2026-02-06 21:41:07 +0530

    Add Q5_StockBuyAndSell_Easy.java solution
    Implement solution for the Best Time to Buy and Sell Stock problem.

---
+++ Arrays/Q5_StockBuyAndSell_Easy.java
@@ -0,0 +1,23 @@
+/**
+ * Question: Best Time to Buy and Sell Stock (LeetCode #121)
+ * Approach:- Iterate through the array while keeping track of the minimum price seen so far to calculate and update the maximum possible profit at each step.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+public class Q5_StockBuyAndSell_Easy {
+    public static int maxProfit(int[] arr) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(buyPrice < arr[i]) {
+                int Profit = arr[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, Profit);
+            }
+            else {
+                buyPrice = arr[i];
+            }
+        }
+        return maxProfit;
+    }
+}
