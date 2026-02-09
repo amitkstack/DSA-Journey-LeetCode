4167c0b3254ee5962e8dc4a717070e99dbd337f4
Amit Kumar <amitkumar128206@gmail.com>
2026-02-09 22:52:47 +0530

    Rename class Q7_SearchIn2dMatrix_Medium to Q12_SearchIn2dMatrix_Medium

--- Arrays/Q12_SearchIn2dMatrix_Medium.java
+++ Arrays/Q12_SearchIn2dMatrix_Medium.java
@@ -5,7 +5,7 @@
  * Space Complexity: O(1)
  */
 
-public class Q7_SearchIn2dMatrix_Medium {
+public class Q12_SearchIn2dMatrix_Medium {
     public boolean searchMatrix(int[][] matrix, int target) {
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[i].length; j++){
